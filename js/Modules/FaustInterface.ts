fa788c8b97003345c7c7d5f6a32e9a42bcd8af4a
Yann Orlarey <orlarey@grame.fr>
2016-10-07 15:51:49 +0200

    Fix Button bug

--- js/Modules/FaustInterface.ts
+++ js/Modules/FaustInterface.ts
@@ -118,10 +118,8 @@ class FaustInterfaceControler {
                 (adress, value) => { this.setDSPValueCallback(adress,value) }
             );
             controler.name = itemElement.label;
-            controler.itemParam = itemElement
+            controler.itemParam = itemElement;
             controler.value = itemElement.init;
-
-
             this.faustControlers.push(controler)
 
 
@@ -132,6 +130,7 @@ class FaustInterfaceControler {
                 (adress, value) => { this.setDSPValueCallback(adress, value) }
             );
             controler.itemParam = itemElement;
+            controler.value = "0";
             this.faustControlers.push(controler)
 
         } else if (item.type === "checkbox") {
@@ -141,6 +140,7 @@ class FaustInterfaceControler {
                 (adress, value) => { this.setDSPValueCallback(adress, value) }
             );
             controler.itemParam = itemElement;
+            controler.value = "0";
             this.faustControlers.push(controler)
 
         }
