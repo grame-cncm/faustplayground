03ffddb4dac72b8bb114b3d8dda7c3e329d70b61
Stéphane Letz <letz@grame.fr>
2026-08-25 19:48:23 +0200

    Updated with faustwasm 0.17.0. Set version to 1.6.0.

--- src/Main.ts
+++ src/Main.ts
@@ -33,7 +33,7 @@ export let faustWasmEnv: {
 //initialization af the app, create app and resource to get text with correct localization
 //then resumeInit on callback when text is loaded
 export async function init(): Promise<void> {
-    console.log("FaustPlayground: version 1.5.1 (2026-08-02)");
+    console.log("FaustPlayground: version 1.6.0 (2026-08-25)");
     //@ts-ignore
     const faustwasm = await import("@grame/faustwasm");
     console.log(faustwasm);
